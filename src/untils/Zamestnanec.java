ca3fb5c58d1c50425ca584d29f5d5c1b25122d12
Aries023 <pet.potocka@gmail.com>
2021-04-09 04:06:44 +0200

    pridanie cisloDavacu a uprava Zamestnanca

--- src/untils/Zamestnanec.java
+++ src/untils/Zamestnanec.java
@@ -1,7 +1,15 @@
 package untils;
 
 public class Zamestnanec {
-    String meno;
-    byte vek;
+    String meno = "este nema meno";
+    String telCislo = "zatial nieje ziadne";
+    String osobneCislo = "zatial nema";
+    int cisloVyrobenychZamestnancov = 0;
 
+    public Zamestnanec(String meno, String telCislo, String osobneCislo) {
+        cisloVyrobenychZamestnancov++;
+        this.meno = meno;
+        this.telCislo = telCislo;
+        this.osobneCislo = osobneCislo;
+    }
 }
