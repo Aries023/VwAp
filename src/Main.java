f65a06aae327def0c392d1a954925eabdf9f6293
Aries023 <pet.potocka@gmail.com>
2021-04-28 01:46:58 +0200

    pridal som do zakladneho okna 2 panely + do prveho panelu som dal nadpis + do druheho 3 tlacitka

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,20 @@ import java.awt.*;
 public class Main {
 
     JFrame window;
+    Container con;
+    JPanel titleNamePanel, startButtomPanel;
+    JLabel titleNameLabel;
+    JButton create, change, split;
+    Font titleFont = new Font("Times New Roman",Font.PLAIN,85);
+    Font normalFont = new Font("Times New Roman",Font.PLAIN,28);
 
     public static void main(String[] args) {
 
         new Main();
     }
     public Main(){
+
+        // Okno = Windiow
         window = new JFrame("Team Builder");
         window.setSize(800,600);
         window.setResizable(false);
@@ -17,6 +25,48 @@ public class Main {
         window.getContentPane().setBackground(Color.LIGHT_GRAY);
         window.setLayout(null);
         window.setVisible(true);
+        con = window.getContentPane();
 
+        // Nadpis = Title
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100,100,600,100);
+        titleNamePanel.setBackground(Color.GRAY);
+        titleNameLabel = new JLabel("Team Builder");
+        titleNameLabel.setForeground(Color.BLUE);
+        titleNameLabel.setFont(titleFont);
+
+        //---------------------------------------------
+        // Tlacitka = Buttoms
+        startButtomPanel = new JPanel();
+        startButtomPanel.setBounds(300,300,200,150);
+        startButtomPanel.setBackground(Color.lightGray);
+
+        // Create / Vytvorit
+        create = new JButton("Create / Vytvorit");
+        create.setBackground(Color.GRAY);
+        create.setForeground(Color.BLUE);
+        create.setFont(normalFont);
+
+        // Change / Upravit
+        change = new JButton("Change / Upravit");
+        change.setBackground(Color.GRAY);
+        change.setForeground(Color.BLUE);
+        change.setFont(normalFont);
+
+        // Split / Rozdelit
+        split = new JButton("Split / Rozdelit");
+        split.setBackground(Color.GRAY);
+        split.setForeground(Color.BLUE);
+        split.setFont(normalFont);
+
+        //------------------------------------------------
+        // Panels Baby
+        titleNamePanel.add(titleNameLabel);
+        startButtomPanel.add(create);
+        startButtomPanel.add(change);
+        startButtomPanel.add(split);
+
+        con.add(titleNamePanel);
+        con.add(startButtomPanel);
     }
 }
