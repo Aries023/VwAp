8161aa88a3372ff4d099e4d3831683941d63b7a5
Aries023 <pet.potocka@gmail.com>
2021-04-27 13:31:45 +0200

    Main = vytvorenie zakladneho okna + vyrieseny problem s gitom pomocou prikazu  git checkout -- .

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
+    JFrame window;
+
     public static void main(String[] args) {
+
+        new Main();
+    }
+    public Main(){
+        window = new JFrame("Team Builder");
+        window.setSize(800,600);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.LIGHT_GRAY);
+        window.setLayout(null);
+        window.setVisible(true);
 
     }
 }
